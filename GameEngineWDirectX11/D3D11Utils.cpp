638f41ea7b847edc1358f3bd40ef6d779dc49ce9
Seungho Jang <sjang1594@gmail.com>
2025-05-29 23:56:40 +0900

    refactor move all the Create Resource & Shaders

---
+++ GameEngineWDirectX11/D3D11Utils.cpp
@@ -0,0 +1,138 @@
+#include "pch.h"
+#include "D3D11Utils.h"
+#include <directxtk/DDSTextureLoader.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_write.h"
+
+namespace Luna {
+
+void CheckResult(HRESULT hr, ID3DBlob *errorBlob) {
+    if (FAILED(hr)) {
+        if ((hr & D3D11_ERROR_FILE_NOT_FOUND) != 0) {
+            std::cout << "File not found." << std::endl;
+        }
+        if (errorBlob) {
+            std::cout << "Shader compile error\n" << (char *)errorBlob->GetBufferPointer() << std::endl;
+        }
+    }
+}
+
+void D3D11Utils::CreateVertexShaderAndInputLayout(
+    ComPtr<ID3D11Device> &device, const wstring &filename,
+    const vector<D3D11_INPUT_ELEMENT_DESC> &inputElements,
+    ComPtr<ID3D11VertexShader> &m_vertexShader, ComPtr<ID3D11InputLayout> &m_inputLayout) {
+    
+    ComPtr<ID3DBlob> shaderBlob;
+    ComPtr<ID3DBlob> errorBlob;
+
+    UINT compileFlags = 0;
+#if defined(DEBUG) || defined(_DEBUG)
+    compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+  
+    HRESULT hr = D3DCompileFromFile(filename.c_str(), 0, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main",
+                                    "vs_5_0", compileFlags, 0, &shaderBlob, &errorBlob);
+
+    CheckResult(hr, errorBlob.Get());
+
+    device->CreateVertexShader(shaderBlob->GetBufferPointer(), shaderBlob->GetBufferSize(), NULL,
+                               &m_vertexShader);
+
+    device->CreateInputLayout(inputElements.data(), UINT(inputElements.size()),
+                              shaderBlob->GetBufferPointer(), shaderBlob->GetBufferSize(),
+                              &m_inputLayout);
+}
+
+void D3D11Utils::CreateHullShader(ComPtr<ID3D11Device> &device, const wstring &filename,
+                                  ComPtr<ID3D11HullShader> &hullShader) {
+    ComPtr<ID3DBlob> shaderBlob;
+    ComPtr<ID3DBlob> errorBlob;
+    
+    UINT compileFlags = 0;
+#if defined(DEBUG) || defined(_DEBUG)
+    compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+    HRESULT hr = D3DCompileFromFile(filename.c_str(), 0, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main",
+                                    "hs_5_0", compileFlags, 0, &shaderBlob, &errorBlob);
+    CheckResult(hr, errorBlob.Get());
+    device->CreateHullShader(shaderBlob->GetBufferPointer(), shaderBlob->GetBufferSize(), NULL,
+                             &hullShader);
+}
+
+void D3D11Utils::CreateDomainShader(ComPtr<ID3D11Device> &device, const wstring &filename,
+                                    ComPtr<ID3D11DomainShader> &domainShader) {
+    ComPtr<ID3DBlob> shaderBlob;
+    ComPtr<ID3DBlob> errorBlob;
+
+    UINT compileFlags = 0;
+#if defined(DEBUG) || defined(_DEBUG)
+    compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+    HRESULT hr = D3DCompileFromFile(filename.c_str(), 0, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main",
+                                    "ds_5_0", compileFlags, 0, &shaderBlob, &errorBlob);
+
+    CheckResult(hr, errorBlob.Get());
+
+    device->CreateDomainShader(shaderBlob->GetBufferPointer(), shaderBlob->GetBufferSize(), NULL,
+                               &domainShader);
+}
+
+void D3D11Utils::CreateGeometryShader(ComPtr<ID3D11Device> &device, const wstring &filename,
+                                      ComPtr<ID3D11GeometryShader> &geometryShader) {
+    ComPtr<ID3DBlob> shaderBlob;
+    ComPtr<ID3DBlob> errorBlob;
+    
+    UINT compileFlags = 0;
+#if defined(DEBUG) || defined(_DEBUG)
+    compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+    HRESULT hr = D3DCompileFromFile(filename.c_str(), 0, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main",
+                                    "gs_5_0", compileFlags, 0, &shaderBlob, &errorBlob);
+
+    device->CreateGeometryShader(shaderBlob->GetBufferPointer(), shaderBlob->GetBufferSize(), NULL,
+                                 &geometryShader);
+}
+
+void D3D11Utils::CreatePixelShader(ComPtr<ID3D11Device> &device, const wstring &filename,
+                                   ComPtr<ID3D11PixelShader> &pixelShader) {
+    ComPtr<ID3DBlob> shaderBlob;
+    ComPtr<ID3DBlob> errorBlob;
+
+    UINT compileFlags = 0;
+#if defined(DEBUG) || defined(_DEBUG)
+    compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+    HRESULT hr = D3DCompileFromFile(filename.c_str(), 0, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main",
+                                    "ps_5_0", compileFlags, 0, &shaderBlob, &errorBlob);
+
+    CheckResult(hr, errorBlob.Get());
+
+    device->CreatePixelShader(shaderBlob->GetBufferPointer(), shaderBlob->GetBufferSize(), NULL,
+                              &pixelShader);
+}
+
+void D3D11Utils::CreateIndexBuffer(ComPtr<ID3D11Device> &device, const vector<uint32_t> &indices,
+                                   ComPtr<ID3D11Buffer> &indexBuffer) {
+    D3D11_BUFFER_DESC bufferDesc = {};
+    bufferDesc.Usage = D3D11_USAGE_IMMUTABLE; // Don't Change After Init
+    bufferDesc.ByteWidth = UINT(sizeof(uint32_t) * indices.size());
+    bufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    bufferDesc.CPUAccessFlags = 0; // 0 if no CPU access is necessary.
+    bufferDesc.StructureByteStride = sizeof(uint32_t);
+
+    D3D11_SUBRESOURCE_DATA indexBufferData = {0};
+    indexBufferData.pSysMem = indices.data();
+    indexBufferData.SysMemPitch = 0;
+    indexBufferData.SysMemSlicePitch = 0;
+
+    device->CreateBuffer(&bufferDesc, &indexBufferData, indexBuffer.GetAddressOf());
+}
+
+} // namespace Luna
---
+++ GameEngineWDirectX11/D3D11Utils.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <wrl/client.h> // ComPtr
+
+namespace Luna {
+using Microsoft::WRL::ComPtr;
+using std::shared_ptr;
+using std::vector;
+using std::wstring;
+
+inline void ThrowIfFailed(HRESULT hr) {
+    if (FAILED(hr)) {
+        throw std::exception();
+    }
+}
+
+class D3D11Utils {
+  public:
+    static void CreateVertexShaderAndInputLayout(
+        ComPtr<ID3D11Device> &device, const wstring &filename,
+        const vector<D3D11_INPUT_ELEMENT_DESC> &inputElements,
+        ComPtr<ID3D11VertexShader> &m_vertexShader,
+        ComPtr<ID3D11InputLayout> &m_inputLayout);
+
+    static void CreateHullShader(ComPtr<ID3D11Device> &device, const wstring &filename,
+                                 ComPtr<ID3D11HullShader> &hullShader);
+
+    static void CreateDomainShader(ComPtr<ID3D11Device> &device, const wstring &filename,
+                                   ComPtr<ID3D11DomainShader> &domainShader);
+
+    static void CreateGeometryShader(ComPtr<ID3D11Device> &device, const wstring &filename,
+                                     ComPtr<ID3D11GeometryShader> &geometryShader);
+
+    static void CreatePixelShader(ComPtr<ID3D11Device> &device, const wstring &filename,
+                                  ComPtr<ID3D11PixelShader> &pixelShader);
+
+    template <typename T>
+    static void CreateVertexBuffer(ComPtr<ID3D11Device> &device, const vector<T> &vertices,
+                                   ComPtr<ID3D11Buffer> &vertexBuffer);
+
+    static void CreateIndexBuffer(ComPtr<ID3D11Device> &device, const vector<uint32_t> &indices,
+                                  ComPtr<ID3D11Buffer> &indexBuffer);
+
+    template <typename T> 
+    static void CreateConstantBuffer(ComPtr<ID3D11Device> &device, const T &constantBufferData,
+                                     ComPtr<ID3D11Buffer> &constantBuffer);
+
+    template <typename T>
+    static void UpdateBuffer(ComPtr<ID3D11Device> &device, ComPtr<ID3D11DeviceContext> &context,
+                             const T &bufferData, ComPtr<ID3D11Buffer> &buffer);
+
+    static void CreateTexture(ComPtr<ID3D11Device> &device, ComPtr<ID3D11DeviceContext> &context,
+                              const std::string filename, const bool useSRGB,
+                              ComPtr<ID3D11Texture2D> &texture,
+                              ComPtr<ID3D11ShaderResourceView> &srv) {}
+
+    static void CreateMetallicRoughnessTexture(ComPtr<ID3D11Device> &device,
+                                               ComPtr<ID3D11DeviceContext> &context,
+                                               const std::string metallicFileName,
+                                               const std::string roughnessFileName,
+                                               ComPtr<ID3D11Texture2D> &texture,
+                                               ComPtr<ID3D11ShaderResourceView> &srv) {}
+
+    static void CreateTextureArray(ComPtr<ID3D11Device> &device,
+                                   ComPtr<ID3D11DeviceContext> &context,
+                                   const std::vector<std::string> filenames,
+                                   ComPtr<ID3D11Texture2D> &texture,
+                                   ComPtr<ID3D11ShaderResourceView> &srv) {}
+
+    static void CreateDDSTexture(ComPtr<ID3D11Device> &device, const wchar_t *fileName,
+                                 const bool isCubeMap, ComPtr<ID3D11ShaderResourceView> &srv) {}
+};
+
+template <typename T>
+inline void D3D11Utils::CreateVertexBuffer(ComPtr<ID3D11Device> &device, const vector<T> &vertices,
+                                           ComPtr<ID3D11Buffer> &vertexBuffer) {
+    D3D11_BUFFER_DESC bufferDesc;
+    ZeroMemory(&bufferDesc, sizeof(bufferDesc));
+    bufferDesc.Usage = D3D11_USAGE_IMMUTABLE; // Don't Change After Init
+    bufferDesc.ByteWidth = UINT(sizeof(T_VERTEX) * vertices.size());
+    bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    bufferDesc.CPUAccessFlags = 0; // 0 if no CPU access is necessary.
+    bufferDesc.StructureByteStride = sizeof(T_VERTEX);
+
+    D3D11_SUBRESOURCE_DATA vertexBufferData = {0};
+    vertexBufferData.pSysMem = vertices.data();
+    vertexBufferData.SysMemPitch = 0;
+    vertexBufferData.SysMemSlicePitch = 0;
+
+    ThrowIfFailed(
+        device->CreateBuffer(&bufferDesc, &vertexBufferData, vertexBuffer.GetAddressOf()));
+}
+
+template <typename T>
+inline void D3D11Utils::CreateConstantBuffer(ComPtr<ID3D11Device> &device,
+                                             const T &constantBufferData,
+                                             ComPtr<ID3D11Buffer> &constantBuffer) {
+    // NOTE
+    static_assert((sizeof(T_CONSTANT) % 16) == 0, "Constant Buffer size must be 16-byte aligned");
+
+    D3D11_BUFFER_DESC desc;
+    ZeroMemory(&desc, sizeof(desc));
+    desc.ByteWidth = sizeof(constantBufferData);
+    desc.Usage = D3D11_USAGE_DYNAMIC;
+    desc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+    desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+    desc.MiscFlags = 0;
+    desc.StructureByteStride = 0;
+
+    D3D11_SUBRESOURCE_DATA initData;
+    ZeroMemory(&initData, sizeof(initData));
+    initData.pSysMem = &constantBufferData;
+    initData.SysMemPitch = 0;
+    initData.SysMemSlicePitch = 0;
+
+    ThrowIfFailed(device->CreateBuffer(&desc, &initData, constantBuffer.GetAddressOf()));
+}
+
+template <typename T>
+inline void D3D11Utils::UpdateBuffer(ComPtr<ID3D11Device> &device,
+                                     ComPtr<ID3D11DeviceContext> &context, const T &bufferData,
+                                     ComPtr<ID3D11Buffer> &buffer) {
+    if (!buffer) {
+        std::cout << "UpdateBuffer() buffer was not initialized." << std::endl;
+    }
+
+    D3D11_MAPPED_SUBRESOURCE ms;
+    context->Map(buffer.Get(), NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);
+    memcpy(ms.pData, &bufferData, sizeof(bufferData));
+    context->Unmap(buffer.Get(), NULL);
+}
+} // namespace Luna
